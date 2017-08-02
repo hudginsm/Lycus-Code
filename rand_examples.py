5bfd33840a10c35d311568a41a192f11b1c68a6b
hudginsm <hudginsm@live.com>
2017-08-02 14:27:05 -0500

    Create rand_examples.py

---
+++ rand_examples.py
@@ -0,0 +1,32 @@
+##from random import randint
+##i=int
+##for i in range (0,6):
+##    print(randint(1,6))
+    
+##import random
+##print(random.randint(0,9))
+
+##from random import randrange, uniform
+###randrange gives you an inetgral value
+##irand = randrange(0,10)
+###uniform gives you a floating-point value
+##frand = uniform(0,10)
+##print(irand)
+##print(frand)
+
+##from random import randint
+##x=[randint(0,9) for p in range(0,9)]
+##print(x)
+
+##import random
+##print(random.randrange(10))
+
+##import random
+##nums = [x for x in range(10)]
+##random.shuffle(nums)
+##print(nums)
+
+##from random import randrange
+##random_number = randrange(10)
+### from 0 to 9 and 10 is not included
+##print(random_number)
