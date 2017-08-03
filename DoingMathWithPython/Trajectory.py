9470323a95d90734868c77f671cefa425795124e
hudginsm <22251133+hudginsm@users.noreply.github.com>
2017-08-03 11:32:19 -0500

    Create Trajectory.py

---
+++ DoingMathWithPython/Trajectory.py
@@ -0,0 +1,44 @@
+'''
+Draw the trajectory of a body in projectile motion
+'''
+
+from matplotlib import pyplot as plt
+import math
+
+def draw_graph(x, y):
+    plt.plot(x, y)
+    plt.xlabel('x-coordinate')
+    plt.ylabel('y-coordinate')
+    plt.title('Projectile Motion of a Ball')
+    plt.show()
+
+def frange(start, final, interval):
+    numbers = []
+    while start < final:
+        numbers.append(start)
+        start = start + interval
+    return numbers
+
+def draw_trajectory(u, theta):
+    theta = math.radians(theta)
+    g = 9.8
+    #Time of flight
+    t_flight = 2*u*math.sin(theta)/g
+    #find time intervals
+    intervals = frange(0, t_flight, 0,001)
+    #list of x and y coordinates
+    x = []
+    y = []
+    for t in intervals:
+        x.append(u*math.cos(theta)*t)
+        y.append(u*math.cos(theta)*t - 0.5*g*t*t)
+    draw_graph(x, y)
+
+if __name__ == '__main__':
+    try:
+        u = float(input('Enter the inital velocity (m/s): '))
+        theta = float(input('Enter the angle of projection (degrees): '))
+    except:
+        print('You entered an invalid input!')
+    else:
+        draw_trajectory(u, theta)
